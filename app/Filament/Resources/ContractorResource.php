50e41bae6765547fef741341322dff2b4dec5331
Michael Konabiyovwi <konamike22@gmail.com>
2023-10-19 16:51:41 +0100

    Initial Commit

---
+++ app/Filament/Resources/ContractorResource.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ContractorResource\Pages;
+use App\Filament\Resources\ContractorResource\RelationManagers;
+use App\Models\Contractor;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\TextColumn;
+
+class ContractorResource extends Resource
+{
+    protected static ?string $model = Contractor::class;
+
+    protected static ?string $navigationIcon = 'heroicon-s-briefcase';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')->required(),
+                TextInput::make('contact person'),
+                TextInput::make('email')->email(),
+                TextInput::make('phone'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id'),
+                TextColumn::make('name'),
+                TextColumn::make('email'),
+                TextColumn::make('contact person'),
+                TextColumn::make('phone'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListContractors::route('/'),
+            'create' => Pages\CreateContractor::route('/create'),
+            'edit' => Pages\EditContractor::route('/{record}/edit'),
+        ];
+    }    
+}
